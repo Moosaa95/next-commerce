05097570e4b3a739d643494e25ebe1c5b0cfa87e
moosa <moosaabdullahi45@gmail.com>
2024-01-03 15:35:56 +0200

    connect db with prisma and turso

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,8 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+
+  
   return (
     <ClerkProvider>
     <html lang="en">
---
+++ prisma/migrations/20240103133123_ecommerce_new/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "Store" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updateAt" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Team" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updateAt" DATETIME NOT NULL
+);
